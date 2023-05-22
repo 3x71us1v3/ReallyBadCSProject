d9b54bd4b7b33fb8e7e20d4c6bb517045d0b57b1
3x71us1v3 <you@example.com>
2023-05-21 20:23:51 -0700

    :)

---
+++ main.py
@@ -0,0 +1,9 @@
+
+#Sorry, I realize that this is really half-assed, I had a busy week :(
+
+def start():
+    inputLol = int(input("Give me the number:"))
+    print(bin(inputLol)[2:])
+    start()
+
+start()
